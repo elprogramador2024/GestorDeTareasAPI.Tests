0d968e26f29449087411f836a52628a4a4158a01
elprogramador2024 <jthmacias1995@gmail.com>
2024-10-20 20:34:51 -0500

    first commit

---
+++ TareaControllerTest.cs
@@ -0,0 +1,188 @@
+﻿using GestorDeTareas.Controllers;
+using GestorDeTareas.Models;
+using GestorDeTareas.Models.Custom;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System.Security.Claims;
+
+namespace GestorDeTareas.Tests
+{
+    public class TareaControllerTest
+    {
+        private readonly Mock<UserManager<ApplicationUser>> _userManagerMock;
+        private readonly Mock<RoleManager<IdentityRole>> _roleManagerMock;
+        private readonly ApplicationDbContext _db;
+        private readonly TareaController _controller;
+        
+        public TareaControllerTest()
+        {
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            _userManagerMock = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
+
+            var roleStoreMock = new Mock<IRoleStore<IdentityRole>>();
+            _roleManagerMock = new Mock<RoleManager<IdentityRole>>(roleStoreMock.Object, null, null, null, null);
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                  . UseInMemoryDatabase(databaseName: "TestDB")
+                  .Options;
+
+            _db = new ApplicationDbContext(options);            
+
+            _controller = new TareaController(_userManagerMock.Object, _roleManagerMock.Object, _db, null);
+
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Role, "Administrador"),
+                    new Claim(ClaimTypes.Name, "admin")
+                    }, "TestAuth"))
+                }
+            };
+        }
+
+        [Fact]
+        public async Task GetAll()
+        {
+            var usuario = new ApplicationUser { UserName = "admin", Id = "1" };
+
+            _userManagerMock.Setup(um => um.FindByNameAsync(usuario.UserName))
+                            .ReturnsAsync(usuario);
+            _userManagerMock.Setup(um => um.CheckPasswordAsync(usuario, "Admin052@"))
+                            .ReturnsAsync(true);
+            _userManagerMock.Setup(um => um.GetRolesAsync(usuario))
+                            .ReturnsAsync(new List<string> { "Administrador" });
+
+            Tarea tarea = new Tarea { Id = 1, Titulo = "Tarea 1", Descripcion = "Descripción Tarea 1", FechaIni = new DateTime(), FechaFin = new DateTime(), UserName = "admin", Estado = EstadoTarea.PENDIENTE };
+            _db.Tareas.Add(tarea);
+            _db.SaveChanges();
+
+            int pgnum = 1; 
+            int pgsize = 5;
+
+            var result = await _controller.GetAll(pgnum, pgsize);
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            var tareas = Assert.IsType<PgResponse>(ok_result.Value);
+            Assert.Contains(tarea, tareas.Tareas);
+        }
+
+        
+        [Fact]
+        public async Task GetByUser()
+        {
+            var usuario = new ApplicationUser { UserName = "user1", Id = "2" };
+
+            _userManagerMock.Setup(um => um.FindByNameAsync(usuario.UserName))
+                            .ReturnsAsync(usuario);
+            _userManagerMock.Setup(um => um.CheckPasswordAsync(usuario, "User078@"))
+                            .ReturnsAsync(true);
+            _userManagerMock.Setup(um => um.GetRolesAsync(usuario))
+                            .ReturnsAsync(new List<string> { "Empleado" });
+        
+            Tarea tarea = new Tarea { Id = 2, Titulo = "Tarea 2", Descripcion = "Descripción Tarea para Empleado", FechaIni = new DateTime(), FechaFin = new DateTime(), UserName = usuario.UserName, Estado = EstadoTarea.PENDIENTE };
+            _db.Tareas.Add(tarea);
+            _db.SaveChanges();
+
+            int pgnum = 1;
+            int pgsize = 5;
+
+            var result = await _controller.GetByUser(usuario.UserName, pgnum, pgsize);
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            var tareas = Assert.IsType<PgResponse>(ok_result.Value);
+            Assert.All(tareas.Tareas, tarea => Assert.Equal(usuario.UserName, tarea.UserName));
+        }
+
+        [Fact]
+        public async Task Insert()
+        {
+            var usuario = new ApplicationUser { UserName = "user2", Id = "3" };
+
+            _userManagerMock.Setup(um => um.FindByNameAsync(usuario.UserName))
+                            .ReturnsAsync(usuario);
+            _userManagerMock.Setup(um => um.CheckPasswordAsync(usuario, "User452@"))
+                            .ReturnsAsync(true);
+            _userManagerMock.Setup(um => um.GetRolesAsync(usuario))
+                            .ReturnsAsync(new List<string> { "Empleado" });
+
+            var model = new Tarea { Id = 3, Titulo = "Tarea 3", Descripcion = "Descripción Tarea 3", FechaIni = new DateTime(), FechaFin = new DateTime(), UserName = usuario.UserName, Estado = EstadoTarea.PENDIENTE };
+
+            var result = await _controller.Insert(model);
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Tarea creada exitosamente!", ok_result.Value.GetType().GetProperty("message").GetValue(ok_result.Value));
+        }
+
+        
+        [Fact]
+        public async Task Update()
+        {
+            var usuario = new ApplicationUser { UserName = "user2", Id = "3" };
+
+            _userManagerMock.Setup(um => um.FindByNameAsync(usuario.UserName))
+                            .ReturnsAsync(usuario);
+            _userManagerMock.Setup(um => um.CheckPasswordAsync(usuario, "User452@"))
+                            .ReturnsAsync(true);
+            _userManagerMock.Setup(um => um.GetRolesAsync(usuario))
+                            .ReturnsAsync(new List<string> { "Empleado" });
+
+            var model = new Tarea { Id = 3, Titulo = "Tarea 3", Descripcion = "Descripción Tarea 3 Actualizada!", FechaIni = new DateTime(), FechaFin = new DateTime(), UserName = usuario.UserName, Estado = EstadoTarea.PENDIENTE };
+
+            var result = await _controller.Update(model);
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Tarea actualizada exitosamente!", ok_result.Value.GetType().GetProperty("message").GetValue(ok_result.Value));
+        }
+
+        
+        [Fact]
+        public async Task UpdateEstado()
+        {
+            var usuario = new ApplicationUser { UserName = "user2", Id = "3" };
+
+            _userManagerMock.Setup(um => um.FindByNameAsync(usuario.UserName))
+                            .ReturnsAsync(usuario);
+            _userManagerMock.Setup(um => um.CheckPasswordAsync(usuario, "User452@"))
+                            .ReturnsAsync(true);
+            _userManagerMock.Setup(um => um.GetRolesAsync(usuario))
+                            .ReturnsAsync(new List<string> { "Empleado" });
+
+            var model = new Tarea { Id = 3, Titulo = "Tarea 3", Descripcion = "Descripción Tarea 3", FechaIni = new DateTime(), FechaFin = new DateTime(), UserName = usuario.UserName, Estado = EstadoTarea.ENPROCESO };
+
+            var result = await _controller.UpdateEstado(model);
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Estado de la Tarea actualizado exitosamente!", ok_result.Value.GetType().GetProperty("message").GetValue(ok_result.Value));
+        }
+
+        
+        [Fact]
+        public async Task Delete()
+        {
+            var usuario = new ApplicationUser { UserName = "user2", Id = "3" };
+
+            _userManagerMock.Setup(um => um.FindByNameAsync(usuario.UserName))
+                            .ReturnsAsync(usuario);
+            _userManagerMock.Setup(um => um.CheckPasswordAsync(usuario, "User452@"))
+                            .ReturnsAsync(true);
+            _userManagerMock.Setup(um => um.GetRolesAsync(usuario))
+                            .ReturnsAsync(new List<string> { "Empleado" });
+
+            Tarea tarea = new Tarea { Id = 4, Titulo = "Tarea 4", Descripcion = "Descripción Tarea 4", FechaIni = new DateTime(), FechaFin = new DateTime(), UserName = usuario.UserName, Estado = EstadoTarea.PENDIENTE };
+            _db.Tareas.Add(tarea);
+            _db.SaveChanges(); ;
+
+            var result = await _controller.Delete(tarea.Id);
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Tarea eliminada exitosamente!", ok_result.Value.GetType().GetProperty("message").GetValue(ok_result.Value));
+        }
+        
+
+    }
+}
---
+++ UsuarioControllerTest.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using Xunit;
+using GestorDeTareas.Models;
+using GestorDeTareas.Controllers;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace GestorDeTareas.Tests
+{
+    public class UsuarioControllerTest
+    {
+        private readonly Mock<UserManager<ApplicationUser>> _userManagerMock;
+        private readonly Mock<RoleManager<IdentityRole>> _roleManagerMock;
+        private readonly UsuarioController _controller;
+
+        public UsuarioControllerTest()
+        {
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            _userManagerMock = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
+
+            var roleStoreMock = new Mock<IRoleStore<IdentityRole>>();
+            _roleManagerMock = new Mock<RoleManager<IdentityRole>>(roleStoreMock.Object, null, null, null, null);
+
+            _controller = new UsuarioController(_userManagerMock.Object, _roleManagerMock.Object, null);
+
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                    {
+                    new Claim(ClaimTypes.Role, "Administrador"),
+                    new Claim(ClaimTypes.Name, "admin")
+                    }, "TestAuth"))
+                }
+            };
+        }
+
+        [Fact]
+        public async Task GetAll()
+        {
+            var list_usuarios = new List<ApplicationUser>
+            {
+            new ApplicationUser { UserName = "admin", Email = "admin@gmail.com" },
+            new ApplicationUser { UserName = "user1", Email = "user1@gmail.com" }
+            };
+
+            _userManagerMock.Setup(um => um.Users).Returns(list_usuarios.AsQueryable());
+
+            _userManagerMock.Setup(um => um.GetRolesAsync(It.IsAny<ApplicationUser>()))
+                .ReturnsAsync(new List<string> { "Administrador" });
+
+            var result = await _controller.GetAll();
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            var usuarios = Assert.IsType<List<Usuario>>(ok_result.Value);
+
+            Assert.NotNull(usuarios);
+            Assert.NotEmpty(usuarios);
+            Assert.Equal(2, usuarios.Count);
+
+            Assert.Contains(usuarios, u => u.Nombre == "admin" && u.Email == "admin@gmail.com");
+            Assert.Contains(usuarios, u => u.Nombre == "user1" && u.Email == "user1@gmail.com");
+        }
+
+        [Fact]
+        public async Task Insert()
+        {
+            var model = new RegisterUser
+            {
+                Nombre = "user2",
+                Email = "user2@gmail.com",
+                Password = "User2784@",
+                Rol = new Rol { Name = "Empleado" }
+            };
+
+            _roleManagerMock.Setup(rm => rm.RoleExistsAsync(model.Rol.Name))
+                .ReturnsAsync(true);
+
+            _userManagerMock.Setup(um => um.CreateAsync(It.IsAny<ApplicationUser>(), model.Password))
+                .ReturnsAsync(IdentityResult.Success);
+
+            _userManagerMock.Setup(um => um.AddToRoleAsync(It.IsAny<ApplicationUser>(), model.Rol.Name))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var result = await _controller.Insert(model);
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<Microsoft.AspNetCore.Mvc.OkObjectResult>(result).Value;
+            Assert.Equal("Usuario creado exitosamente!", response.GetType().GetProperty("message").GetValue(response));
+        }
+
+        [Fact]
+        public async Task Update()
+        {
+            // Arrange
+            var model = new Usuario
+            {
+                Nombre = "user3",
+                Email = "user3@gmail.com",
+                Rol = new Rol { Name = "Supervisor" }
+            };
+
+            _roleManagerMock.Setup(rm => rm.RoleExistsAsync(model.Rol.Name))
+                .ReturnsAsync(true);
+
+            var user = new ApplicationUser { UserName = model.Nombre, Email = "user3nuevo@gmail.com" };
+            _userManagerMock.Setup(um => um.FindByNameAsync(model.Nombre))
+                .ReturnsAsync(user);
+
+            _userManagerMock.Setup(um => um.UpdateAsync(user))
+                .ReturnsAsync(IdentityResult.Success);
+
+            _userManagerMock.Setup(um => um.AddToRoleAsync(user, model.Rol.Name))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var result = await _controller.Update(model);
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<Microsoft.AspNetCore.Mvc.OkObjectResult>(result).Value;
+            Assert.Equal("Usuario actualizado exitosamente!", response.GetType().GetProperty("message").GetValue(response));
+        }
+
+        [Fact]
+        public async Task Delete()
+        {
+            var model = new Usuario { Nombre = "user4" };
+
+            var user = new ApplicationUser { UserName = model.Nombre };
+            _userManagerMock.Setup(um => um.FindByNameAsync(model.Nombre))
+                .ReturnsAsync(user);
+
+            _userManagerMock.Setup(um => um.DeleteAsync(user))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var result = await _controller.Delete(model);
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsType<Microsoft.AspNetCore.Mvc.OkObjectResult>(result).Value;
+            Assert.Equal("Usuario eliminado exitosamente!", response.GetType().GetProperty("message").GetValue(response));
+        }
+
+        [Fact]
+        public async Task Login()
+        {
+            var user = new ApplicationUser { UserName = "admin", Id = "1" };
+
+            _userManagerMock.Setup(um => um.FindByNameAsync("admin"))
+                            .ReturnsAsync(user);
+            _userManagerMock.Setup(um => um.CheckPasswordAsync(user, "Admin052@"))
+                            .ReturnsAsync(true);
+            _userManagerMock.Setup(um => um.GetRolesAsync(user))
+                            .ReturnsAsync(new List<string> { "Administrador" });
+
+            var _controller = new UsuarioController(_userManagerMock.Object, null, null);
+
+            var result = await _controller.Login(new Login { Name = "admin", Password = "Admin052@" });
+
+            var ok_result = Assert.IsType<OkObjectResult>(result);
+            var response = ok_result.Value;
+            Assert.NotNull(response);
+        }
+    }
+}
+
+
